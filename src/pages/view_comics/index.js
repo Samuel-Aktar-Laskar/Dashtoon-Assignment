e5e822677101f97d421b2fe148664997e5ec1a71
Samuel Aktar Laskar <samuellaskar3@gmail.com>
2023-11-18 17:34:02 +0530

    everything responsive

--- src/pages/view_comics/index.js
+++ src/pages/view_comics/index.js
@@ -3,9 +3,19 @@ import { useContext, useState } from "react"
 
 export default function Home(){
     const context = useContext(AppContext)
-    const sizes = [2,1,1,2,1,1,1,1,2,1,1]
-    const sizesLg=[3,4,4,3,2,3,2,2,2,3]
-    const sizesMd = [1,1,1,2,1,1,2,1,1,1]
+    const mat = [
+        'col-span-2 md:col-span-1 lg:col-span-3 ',
+  'col-span-1 md:col-span-1 lg:col-span-4 ',
+  'col-span-1 md:col-span-1 lg:col-span-4 ',
+  'col-span-2 md:col-span-2 lg:col-span-3 ',
+    'col-span-1 md:col-span-1 lg:col-span-2 ',
+  'col-span-1 md:col-span-1 lg:col-span-3 ',
+  'col-span-1 md:col-span-2 lg:col-span-2 ',
+  'col-span-1 md:col-span-1 lg:col-span-2 ',
+  'col-span-2 md:col-span-1 lg:col-span-2 ',
+  'col-span-1 md:col-span-1 lg:col-span-3 ',
+  'col-span-1 md:col-span-1 lg:col-span-1 '
+    ]
 
     return <section className="mx-auto">
         <div className="grid mx-auto w-[20rem] gap-2 border-[2px] border-gray-300 justify-center p-2 
@@ -16,9 +26,9 @@ export default function Home(){
             {context.comicsList.map((value,key)=>
             <img src={value.url} key={key} className={`
             h-[12rem] w-[20rem] border-2 border-gray-400
-            col-span-${sizes[key]}
-            md:col-span-${sizesMd[key]} md:w-full
-            lg:w-full lg:h-[16rem] lg:col-span-${sizesLg[key]} `
+            md:w-full
+            lg:w-full lg:h-[16rem] 
+            ${mat[key]}`  
         }/>
             )}
         </div>
